eb8ee7489126e601bcb39922150ced98da9b2bed
Sam Rubin <sam.m.rubin@gmail.com>
2024-07-30 20:37:12 -0400

    Fix issue with npm start overriding webpack out assets

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: [
-        './js/bootstrap.js',
-        './js/custom.js',
-        './js/gmap3.js',
-        './js/owl.carousel.js'
+        './js/src/bootstrap.js',
+        './js/src/custom.js',
+        './js/src/gmap3.js',
+        './js/src/owl.carousel.js'
     ],
     output: {
         filename: 'all.min.js',
-        path: path.resolve(__dirname, '_site/js'),
+        path: path.resolve(__dirname, 'js/dist'),
     },
     watch: true,
 };
